64223331e7f7d53d209399777f29a24deb01f868
Mansi <mk.sukhanandi@gmail.com>
2026-07-16 15:37:17 +0530

    Update 01_max_consecutive_ones.py

--- Array/01_max_consecutive_ones.py
+++ Array/01_max_consecutive_ones.py
@@ -1,1 +1,15 @@
+def max_consecutive(num):
+    current_count = 0
+    max_count = 0
+    for n in num :
+        if n == 1:
+            current_count += 1
+            if current_count > max_count :
+                max_count = current_count
+        else :
+            current_count = 0
 
+    return max_count
+
+test_array = [1,1,0,1,1,1,1]
+print("Result :",max_consecutive(test_array))
